92bdecab0fdfd6a3c4cb87a5b2cdc6ded5befcee
masaya48 <msya0408@gmail.com>
2026-04-03 23:59:34 +0900

    chore: add ESLint with Next.js and NestJS specific rules

---
+++ apps/api/eslint.config.mjs
@@ -0,0 +1,28 @@
+import tsPlugin from "@typescript-eslint/eslint-plugin";
+import tsParser from "@typescript-eslint/parser";
+
+export default [
+  {
+    ignores: ["dist/**", "node_modules/**"],
+  },
+  {
+    files: ["**/*.ts"],
+    languageOptions: {
+      parser: tsParser,
+      parserOptions: {
+        project: "./tsconfig.json",
+        tsconfigRootDir: import.meta.dirname,
+      },
+    },
+    plugins: {
+      "@typescript-eslint": tsPlugin,
+    },
+    rules: {
+      "no-unused-vars": "off",
+      "no-console": "off",
+      "eqeqeq": "off",
+      "@typescript-eslint/no-floating-promises": "error",
+      "@typescript-eslint/explicit-function-return-type": "warn",
+    },
+  },
+];
---
+++ apps/web/eslint.config.mjs
@@ -0,0 +1,12 @@
+import nextConfig from "eslint-config-next/core-web-vitals";
+
+export default [
+  ...nextConfig,
+  {
+    rules: {
+      "no-unused-vars": "off",
+      "no-console": "off",
+      "eqeqeq": "off",
+    },
+  },
+];
